6ea0fd53634bb83d281d7c5b85f1e85912037d82
NachitoCode <99271131+NachitoCode@users.noreply.github.com>
2025-03-10 15:21:44 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,8 +6,7 @@ const _ = (eleId) => d.querySelector(`${eleId}`),
 
 // typing animation 
 var typed = new Typed(".typing", {
-  strings: ["Web Designer", "Web Developer", "Graphic Designer"],
-  // strings: ["Web Designer", "Web Developer", "Graphic Designer", "Youtuber"],
+  strings: ["Desarrollador Frontend", "Desarrollador Backend"],
   typeSpeed: 100,
   BackSpeed: 60,
   loop: true
